c05ec9ed7c852793a9a430156184fca53912f336
Abel <abel.lavieri@gmail.com>
2020-10-07 13:47:52 -0400

    Add user controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create]
 
   # GET /posts
   # GET /posts.json
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,5 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+end
