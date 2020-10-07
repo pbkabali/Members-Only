807aabd1d8beedfbc6251e1424a10c86cf36fb16
Abel <abel.lavieri@gmail.com>
2020-10-07 18:56:47 -0400

    Remove duplicated validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :username, :email, :password, presence: true
-  validates :username, :email, uniqueness: true
-  validates :username, :password, length: { in: 4..10 }
 end
