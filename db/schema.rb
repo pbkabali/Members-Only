06df0699cc783e8ea2f468219a51c0cace0bfa0c
Abel <abel.lavieri@gmail.com>
2020-10-07 13:06:42 -0400

    Create posts scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_160851) do
+ActiveRecord::Schema.define(version: 2020_10_07_165717) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
